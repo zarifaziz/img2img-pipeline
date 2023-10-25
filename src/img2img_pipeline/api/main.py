efc41bcb66a2dedb0564a7a61606ab5c215911a5
Zarif Aziz <zarif.aziz@faethm.ai>
2023-10-25 18:59:14 +1100

    fix: code improvements

--- src/img2img_pipeline/api/main.py
+++ src/img2img_pipeline/api/main.py
@@ -8,37 +8,49 @@ import os
 
 from ..model import Img2ImgModel
 from ..pipeline import DiffusionSingleImagePipeline
+from ..constants import INPUT_IMAGE_DIR, OUTPUT_IMAGE_DIR
 
 app = FastAPI()
 
-# Set the path for input and output directories
-input_dir = "data/input_images"
-output_dir = "data/output_images"
-
 # Ensure output directory exists
-os.makedirs(output_dir, exist_ok=True)
+os.makedirs(OUTPUT_IMAGE_DIR, exist_ok=True)
 
 
 # Instantiate the DiffusionSingleImagePipeline during API app startup
 @app.on_event("startup")
 async def startup_event():
+    """
+    This function is called at the startup of the FastAPI application.
+    It initializes the DiffusionSingleImagePipeline with the Img2ImgModel.
+    """
     global pipeline
     pipeline = DiffusionSingleImagePipeline(model_class=Img2ImgModel)
 
 
 @app.post("/images")
 async def generate_image(model_repo: str, prompt: str, image: UploadFile = File(...)):
+    """
+    Generate an image based on the provided model repository and prompt.
+
+    Args:
+        model_repo (str): The model repository to use for image generation.
+        prompt (str): The prompt to guide the image generation.
+        image (UploadFile): The input image file.
+
+    Returns:
+        FileResponse: The generated image.
+    """
     logger.info(
         f"Received request for image generation using {model_repo} repo and prompt: {prompt}"
     )
     # Save the uploaded image to the input directory
-    image_path = os.path.join(input_dir, image.filename)
+    image_path = os.path.join(str(INPUT_IMAGE_DIR), image.filename)
     with open(image_path, "wb") as f:
         f.write(image.file.read())
 
     # Generate a unique filename for the processed image
     output_filename = f"{uuid.uuid4().hex}.jpg"
-    processed_image_path = os.path.join(output_dir, output_filename)
+    processed_image_path = os.path.join(str(OUTPUT_IMAGE_DIR), output_filename)
 
     # Run the diffusion pipeline for the single image with the provided prompt and model
     pipeline.run(image_path, processed_image_path, prompt, model_repo)
